8252ebe41c2d83f0e8677fe154dca4111ede169e
Maria Wuertz <bitrikova.mariia@web.de>
2021-03-01 00:32:48 +0100

    second commit

--- src/components/Content/ProfileInfo/ProfileStatusHook.js
+++ src/components/Content/ProfileInfo/ProfileStatusHook.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import c from './ProfileStatus.module.css'
 
 const ProfileStatusWithHook = (props) => {
 
  let [editMode, setEditMode] = useState(false);
  let [status, setStatus] = useState(props.status);
+
+ useEffect( ()=> {
+     setStatus(props.status);
+ }, [props.status]);
+ 
     let activateEditMode = () => {
         setEditMode(true);
     }
