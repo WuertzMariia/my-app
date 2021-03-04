3ed504b6661625e477ae2708193dabcc2c304516
Maria Wuertz <bitrikova.mariia@web.de>
2021-03-04 12:57:48 +0100

    update styles

--- src/components/Sidebar/Friends/Friends.js
+++ src/components/Sidebar/Friends/Friends.js
@@ -1,29 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import c from './../Sidebar.module.css'
 import s from './Friends.module.css'
 
 const Friends = (props) => {
-
-
-let friendsdata = props.friends.map( f => <div>
-    <img className={s.imageClass} src={f.src}>
-    </img>
-    <NavLink to="/#">{f.name}</NavLink>
-</div>)
-
+    let friendsdata = props.friends.map(f => <div>
+        <img className={s.imageClass} src={f.src}>
+        </img>
+        <NavLink to="/#">{f.name}</NavLink>
+    </div>)
 
     return <div className={c.sidebar_friends}>
-        
-    <h2>Friends</h2>
-    <div className={s.friendsblock}>
-        {friendsdata}
+        <h2>Friends</h2>
+        <div className={s.friendsblock}>
+            {friendsdata}
         </div>
-    
-</div>
-
-      
-    
+    </div>
 }
 
 export default Friends;
